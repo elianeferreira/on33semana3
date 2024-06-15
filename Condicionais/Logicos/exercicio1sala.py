a7d94f8a27cb2f21583150dee763ebdd8bd8ee1e
elianeferreira <sanelisantos@hotmail.com>
2024-06-15 12:04:52 -0300

    exercicios aula semana 3

---
+++ Condicionais/Logicos/exercicio1sala.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+nota = float(input("Digite sua nota: "))
+if nota >= 7:
+  print ("Aprovada")
+else:
+ print ("Reprovada")
---
+++ Condicionais/Logicos/exercicio2sala.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+nota = float(input("Digite sua nota: "))
+presenca = float(input("Digite sua presença: "))
+if nota >= 7 and presenca >= 75:
+     print ("Aprovada")
+else:
+    print ("Reprovada")
