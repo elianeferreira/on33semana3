f3e9d4fafa828616695c1530b4eb1ea9ef2d6b72
elianeferreira <sanelisantos@hotmail.com>
2024-06-18 22:40:17 -0300

    exercicio revisão try/except

---
+++ TryExcept/exemplo3.py
@@ -0,0 +1,43 @@
+Idade = input("digite sua idade: ") 
+
+try:
+    Idade = int(Idade)
+    if Idade <=16 :
+        print("você não vota")
+    else:
+       print("você já pode votar")
+except:
+      print("procure a Justiça eleitoral")      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#idade = input("digite sua idade:")
+ 
+#def idade (numero):
+   # if numero <= 16:
+    # print("você não vota")     
+
+   # else :
+    #   print("você já pode votar")
+
+#try:
+    #idade = int()
+    #print(f"você não vota{idade}")
+#except:
+ #   print(f"Espere a maior{idade}")        
+
+
+      
